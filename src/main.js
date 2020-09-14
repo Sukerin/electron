87df6bc8b8be49737db5f32fcf49f9d2c631edfe
Sukerin <sukerki@163.com>
2020-09-14 11:16:50 +0800

    mdi 本地化

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import router from './router'
 import ECharts from 'vue-echarts' // 在 webpack 环境下指向 components/ECharts.vue
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
 
 // 手动引入 ECharts 各模块来减小打包体积
 import 'echarts'
